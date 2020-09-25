d34cf09f836da9acf4d727321742e396e43371ae
antropod <antropod@gmail.com>
2020-09-25 18:58:56 +0300

    Proper parsing of disconnected player ckey

--- cargo_orders.py
+++ cargo_orders.py
@@ -31,13 +31,32 @@ def match_order(log_line):
     """
     >>> match_order("05:35:25 [0x2032bcd] (152,108,1) || the supply shuttle Order #447 (Janitorial Supplies Crate, placed by test/(Test Tester)), paid by Cargo Budget has shipped.")
     ('447', 'Janitorial Supplies Crate', 'test', 'Test Tester', 'Cargo Budget')
+    
+    >>> match_order("01:17:19 [0x2032d02] (152,108,1) || the supply shuttle Order #8595 (Imported Vending Machines, placed by test_ckey[DC]), paid by Cargo Budget has shipped.")
+    ('8595', 'Imported Vending Machines', 'test_ckey', None, 'Cargo Budget')
     """
-    m = re.search(r"Order #(\d+) \((.+), placed by (.+)/\((.+)\)\), paid by (.+) has shipped.", log_line)
+    m = re.search(r"Order #(\d+) \((.+), placed by (.+)\), paid by (.+) has shipped.", log_line)
     shipment_order = 'has shipped' in log_line
     if bool(m) != bool(shipment_order):
         LOG.error("Failed to parse cargo order, check your regex: {}".format(log_line))
+    if not m:
+        return None
+
+    order_id, pack_name, ckey_raw, account_holder = m.groups()
+    
+    ckey, ic_name = None, None
+    m = re.match(r'(.+)/\((.+)\)', ckey_raw)
     if m:
-        return m.groups()
+        ckey, ic_name = m.groups()
+        return order_id, pack_name, ckey, ic_name, account_holder
+
+    # IC name of disconnected players is None
+    m = re.match(r'(.+)\[DC\]', ckey_raw)
+    if m:
+        ckey, ic_name = m.group(1), None
+        return order_id, pack_name, ckey, ic_name, account_holder
+
+    LOG.error("Failed to parse ckey_raw: {}".format(ckey_raw))
     return None
 
 
