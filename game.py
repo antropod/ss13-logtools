11c943018edabea79057835db5c6b35519bec450
antropod <antropod@gmail.com>
2021-02-05 12:09:40 +0300

    round_id parsing

--- game.py
+++ game.py
@@ -1,23 +1,30 @@
-import re
+from collections import namedtuple
 from zipfile import ZipFile
 import io
 import logging
-from collections import namedtuple
+import re
+import sys
 
 logging.basicConfig(level=logging.INFO, format='%(message)s')
 LOG = logging.getLogger(__name__)
 
-GameMessage = namedtuple('GameMessage', 'dt type message')
+GameMessage = namedtuple('GameMessage', 'round_id dt type message')
 
 
 def parse_game_txt(stream):
+    header = next(stream)
+    m = re.match(r'\[([^\]]+)\] Starting up round ID (\d+).', header)
+    _, round_id = m.groups()
+    round_id = int(round_id)
+
     for line in stream:
         line = line.rstrip('\n')
         if line.startswith('-'):
             continue
         m = re.match(r'\[([^\]]+)\] ([^:]+): (.+$)', line)
         if m:
-            yield GameMessage(*m.groups())
+            dt, type_, message = m.groups()
+            yield GameMessage(round_id, dt, type_, message)
         else:
             LOG.warning("Can't parse %s", line)
 
@@ -25,14 +32,14 @@ def parse_game_txt(stream):
 def parse_round_archive(filename):
     with ZipFile(filename, 'r') as zf:
         with zf.open('game.txt') as fp:
-            stream = io.TextIOWrapper(fp, 'utf-8')
+            stream = io.TextIOWrapper(fp, 'latin-1')
             for m in parse_game_txt(stream):
                 yield m
 
 
 def main():
-    for m in parse_round_archive('data/round/round-153311.zip'):
-        print(repr(m))
+    for m in parse_round_archive('data/round-155420.zip'):
+        print(m)
 
 
 if __name__ == '__main__':
